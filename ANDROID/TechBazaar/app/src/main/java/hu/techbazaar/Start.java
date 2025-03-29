c244aa9aabca7265121505d2171d7b806e6fc5fd
Sanchos <roberto19910619@gmail.com>
2025-03-29 15:06:25 +0100

    T

--- ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Start.java
+++ ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Start.java
@@ -55,7 +55,7 @@ public class Start extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-                        home(); // főkép22
+                        home();
                     } else {
                         ch.setText(R.string.fail_sign_in);
                     }
