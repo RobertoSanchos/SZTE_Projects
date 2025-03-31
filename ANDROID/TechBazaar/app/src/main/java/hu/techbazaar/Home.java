9c1aacdd4044b50f62c9c1a7795707465910dba8
Sanchos <roberto19910619@gmail.com>
2025-03-31 19:47:26 +0200

    .

--- ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Home.java
+++ ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Home.java
@@ -1,12 +1,13 @@
 package hu.techbazaar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.MenuItemCompat;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
 
@@ -15,5 +16,26 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+    }
+
+public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem menui = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menui);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.search) return true;
+        else if (item.getItemId() == R.id.cart) return true;
+        else if (item.getItemId() == R.id.settings) return true;
+        else if (item.getItemId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            return true;
+        }
+        else return super.onOptionsItemSelected(item);
     }
 }
